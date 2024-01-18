e5c4dff75e57818048bab1ff8a1efc5d71fdb945
keerthisiva-2002 <73420512+keerthisiva-2002@users.noreply.github.com>
2024-01-18 21:21:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 function addTask(){
     if(inputBox.value === ''){
-        alert("edhachu eludhu!!!");
+        alert("Please enter something!!");
     }
     else{
         let li = document.createElement("li");
